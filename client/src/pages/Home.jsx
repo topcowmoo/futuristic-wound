7fe9c247363f9693029d00d7b0e0a18b773cd37f
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-05-01 22:16:56 -0400

    Added Header and Footer components.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,14 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import HomeBG from "../assets/my-room-bg.png";
+
 const Home = () => {
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
   return (
     <div>
-      <h1>Home</h1>
+      <div className="h-[625px]">
+        <Header></Header>
+        <h1>Home</h1>
+      </div>
     </div>
   );
 };
