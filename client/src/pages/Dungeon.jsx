9c1a23be88596c835992e81d3d31cfb6b3f03c5f
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-05-03 13:32:05 -0400

    Adding dungeon functions

--- client/src/pages/Dungeon.jsx
+++ client/src/pages/Dungeon.jsx
@@ -1,7 +1,17 @@
+import DungeonBG from "../assets/dungeon-bg.png";
+
 const Dungeon = () => {
   return (
-    <div>
-      <div className="h-[625px]"></div>
+    <div className="w-full h-full">
+      <div className="h-[625px]">
+        <img
+          src={DungeonBG}
+          alt="Dungeon Background"
+          className="fixed w-full h-full object-cover overflow-hidden -z-50"
+        />
+        <div className="monsters w-[300px] h-[200px]"></div>
+        <div className="rps w-[300px] h-[200px]"></div>
+      </div>
     </div>
   );
 };
