1870bdbad15ddff5731c0b7ae5c83236c76f068b
Salvatore <mammoliti.sam@gmail.com>
2024-05-03 11:04:35 -0400

    fw91 updated home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,24 @@
+import { useQuery } from "@apollo/client";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import HomeBG from "../assets/my-room-bg.png";
+import { GET_ACTIVE_MONSTER } from "../utils/queries";
 
 const Home = () => {
+  const { loading, error, data } = useQuery(GET_ACTIVE_MONSTER);
+
+  if (loading) return <p>Loading...</p>;
+
+  const activeMonster = data.me.activeMonster;
+
+
   return (
     <div>
-      <div className="h-[625px]">
-        <Header></Header>
-        <h1>Home</h1>
-      </div>
+      <h2>Home</h2>
+      <p>Name: {activeMonster.name}</p>
+      <img src={activeMonster.image} alt={activeMonster.name} />
     </div>
   );
 };
+
 export default Home;
