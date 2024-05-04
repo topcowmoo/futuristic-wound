d8faf240656f2e5e8723541ba6ae0aa9ec44b871
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-05-03 21:25:19 -0400

    Added Dungeon component. Working RPS game system. Win Screen and Lose Screen.

--- client/src/pages/Dungeon.jsx
+++ client/src/pages/Dungeon.jsx
@@ -1,16 +1,36 @@
 import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_ACTIVE_MONSTER } from "../utils/queries";
+import { useNavigate } from "react-router-dom";
+
+import Modal from "../components/RPSModal";
 
 import DungeonBG from "../assets/dungeon-bg.png";
 import BozoOne from "../assets/psucky.svg";
 import BozoTwo from "../assets/plucky.svg";
 
 const Dungeon = () => {
+  const navigate = useNavigate();
+  const { loading, data } = useQuery(GET_ACTIVE_MONSTER);
+  const [isOpen, setIsOpen] = useState(false);
   const [round, setRound] = useState(1);
   const [life, setLife] = useState(3);
   const [winloss, setWinLoss] = useState("");
   const [playerChoice, setPlayerChoice] = useState(null);
   const [monsterChoice, setMonsterChoice] = useState(null);
   const choices = ["rock", "paper", "scissors"];
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const onClickLoseHandler = () => {
+    navigate("/Home");
+  };
 
   const handlePlayerChoice = (choice) => {
     const monsterChoice = choices[Math.floor(Math.random() * choices.length)];
@@ -23,8 +43,7 @@ const Dungeon = () => {
     if (
       (playerChoice === "rock" && monsterChoice === "scissors") ||
       (playerChoice === "paper" && monsterChoice === "rock") ||
-      (playerChoice === "scissors" && monsterChoice === "paper") ||
-      playerChoice === monsterChoice
+      (playerChoice === "scissors" && monsterChoice === "paper")
     ) {
       setWinLoss("win");
       console.log("You Win");
@@ -56,11 +75,11 @@ const Dungeon = () => {
 
     if (life === 0) {
       console.log("Game Over you have lost");
-      window.location.replace("/home");
+      openModal();
     } else if (round === 3) {
       // Here is where logic for capture monster goes
       console.log("id of the monster to capture");
-      window.location.replace("/home");
+      openModal();
     }
 
     console.log("life", life);
@@ -77,9 +96,56 @@ const Dungeon = () => {
     return hearts;
   };
 
+  const activeMonster = data?.me?.activeMonster;
+
   return (
     <div className="w-full h-full">
       <div className="h-[625px] items-center">
+        {round === 3 ? (
+          <Modal isOpen={isOpen} onClose={closeModal}>
+            <div className="bg-slate-800 fixed flex flex-col w-screen h-screen m-auto justify-center items-center z-20 bg-opacity-90 backdrop-blur-sm">
+              <div className="bg-slate-900 w-[350px] h-[400px] flex flex-col justify-center mx-auto items-center rounded-2xl">
+                <h1 className="text-white text-2xl font-bold">You Win!</h1>
+                <p className="mx-8 my-4 text-white text-center">
+                  You beat the monster! Move to the boss?
+                </p>
+                <div className="space-x-4">
+                  {" "}
+                  <button
+                    className="bg-green-800 text-white font-bold px-4 py-[5px] rounded-lg"
+                    onClick={onClickLoseHandler}
+                  >
+                    CONTINUE
+                  </button>
+                  <button
+                    className="bg-red-800 text-white font-bold px-4 py-[5px] rounded-lg"
+                    onClick={onClickLoseHandler}
+                  >
+                    HOME
+                  </button>
+                </div>
+              </div>
+            </div>
+          </Modal>
+        ) : (
+          <Modal isOpen={isOpen} onClose={closeModal}>
+            <div className="bg-slate-800 fixed flex flex-col w-screen h-screen m-auto justify-center items-center z-20 bg-opacity-90 backdrop-blur-sm">
+              <div className="bg-slate-900 w-[350px] h-[400px] flex flex-col justify-center mx-auto items-center rounded-2xl">
+                <h1 className="text-white text-2xl font-bold">Lose</h1>
+                <p className="mx-8 my-4 text-white text-center">
+                  You hurry back to your room with your injured friend!
+                </p>
+                <button
+                  className="bg-green-800 text-white font-bold px-4 py-[5px] rounded-lg"
+                  onClick={onClickLoseHandler}
+                >
+                  OKAY
+                </button>
+              </div>
+            </div>
+          </Modal>
+        )}
+
         <img
           src={DungeonBG}
           alt="Dungeon Background"
@@ -92,15 +158,15 @@ const Dungeon = () => {
               alt="Bozo One"
               className="relative left-[175px] w-[125px] h-[125px]"
             />
-            <div>
+            <div className="flex flex-row items-center space-x-4">
               <img
-                src={BozoTwo}
-                alt="Bozo Two"
+                src={activeMonster.image}
+                alt={activeMonster.name}
                 className="w-[125px] h-[125px]"
               />
               <div className="flex">
                 {lifeBar().map((heart, i) => (
-                  <div key={i} className="w-[30px] h-[30px]  ">
+                  <div key={i} className="w-[40px] h-[40px] mx-2">
                     <img
                       src="https://cdn-icons-png.flaticon.com/512/833/833472.png"
                       alt="Heart"
@@ -114,7 +180,7 @@ const Dungeon = () => {
           <div className="rps flex flex-row flex-wrap mx-auto items-center justify-center w-[350px] h-[200px] [background:rgba(2,48,71,0.75)] rounded-[25px] border-[5px] border-solid border-[#E9BA14] space-x-2">
             <button
               className="w-[100px] h-[50px] shrink-0  [background:#FFB703] shadow-[0px_4px_42px_0px_rgba(0,0,0,0.25)] rounded-[10px] text-white text-xl font-bold"
-              onClick={() => handlePlayerChoice("rock")}
+              onClick={() => handlePlayerChoice("paper")}
             >
               ROCK
             </button>
