0e32a31543495b7d5aa713aa49cf05a6ba449523
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-05-05 17:45:13 -0400

    Added BGs to Home, Starter and Monster Closet.

--- client/src/pages/Closet.jsx
+++ client/src/pages/Closet.jsx
@@ -1,63 +1,85 @@
-import { useState } from 'react';
+import { useState } from "react";
 import PasswordChangeForm from "../components/PasswordChangeForm";
 
-
-import { useQuery } from "@apollo/client";
+import { from, useQuery } from "@apollo/client";
 import { GET_ACTIVE_MONSTER } from "../utils/queries";
 
 import MonsterBoxModal from "../components/MonsterBoxModal";
+import MyRoom from "../assets/my-room-bg.png";
 
 const Closet = () => {
-  const { loading, data } = useQuery(GET_ACTIVE_MONSTER);
+  const { loading, data } = useQuery(GET_ACTIVE_MONSTER, {
+    pollInterval:100
+  });
 
-  console.log('User data:');
-  console.log(data);
-
+  console.log("User data:");
+ 
   const [isChangePasswordOpen, setIsChangePasswordOpen] = useState(false);
 
   const toggleChangePassword = () => {
     setIsChangePasswordOpen((prev) => !prev);
   };
 
+  const handleCloseChangePassword = () => {
+    setIsChangePasswordOpen(false);
+  };
+
   // State to track the modal visibility
-  const [isModalOpen, setIsModalOpen] = useState(false); 
-  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   if (loading) return <h2>Loading...</h2>;
 
-  // Variable to hold the users active monster data 
+  // Variable to hold the users active monster data
   const activeMonster = data?.me?.activeMonster;
-  
+  console.log(activeMonster);
+
   // Function to toggle modal visibility
   const toggleModal = () => {
-    setIsModalOpen(prevState => !prevState);
+    setIsModalOpen((prevState) => !prevState);
   };
 
   // Function to close the modal
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
-  
+
   return (
-    <div className="relative flex flex-col items-center justify-center h-[625px] py-2">
-      <div className="flex justify-center items-center mt-48">
-        <img src={activeMonster.image} alt={activeMonster.name} className="w-48 h-48 flex justify-center items-center" />
-      </div>
-      <button 
-        className="absolute top-36 right-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-amber-600"
-        onClick={toggleModal} 
-      >
-        Monster Box
-      </button>
-      {/* Render MonsterBoxModal component conditionally based on modal visibility state */}
-      {isModalOpen && <MonsterBoxModal onClose={handleCloseModal} /> }
-      
-      <div className="flex flex-col items-end space-y-8 mt-20">
-        <div className="flex flex-col space-y-4">
-          <button className="px-6 py-3 text-lg bg-blue-500 text-white rounded-md hover:bg-amber-600" onClick={toggleChangePassword}>
-            Change Password
-          </button>
-          {/* Render PasswordChangeForm conditionally based on state */}
-          {isChangePasswordOpen && <PasswordChangeForm />}
+    <div className="relative flex flex-col items-center justify-center h-[620px] py-2">
+      <div className="h-[620px] flex items-center flex-col">
+        <img
+          src={MyRoom}
+          alt="Home Background"
+          className="fixed w-full h-full object-cover overflow-hidden -z-50"
+        />
+        <div className="flex justify-center items-center mt-60">
+          <img
+            src={activeMonster.image}
+            alt={activeMonster.name}
+            className="w-48 h-48 flex justify-center items-center"
+          />
+        </div>
+        <button
+          className="absolute top-36 right-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-amber-600"
+          onClick={toggleModal}
+        >
+          Monster Box
+        </button>
+        {/* Render MonsterBoxModal component conditionally based on modal visibility state */}
+        {isModalOpen && <MonsterBoxModal onClose={handleCloseModal} />}
+
+        <div className="flex flex-col items-end space-y-8 mt-20">
+          <div className="flex flex-col space-y-4">
+            <button
+              className="px-6 py-3 text-lg bg-blue-500 text-white rounded-md hover:bg-amber-600"
+              onClick={toggleChangePassword}
+            >
+              Change Password
+            </button>
+            {/* Render PasswordChangeForm conditionally based on state */}
+            {isChangePasswordOpen && (
+              <PasswordChangeForm onClose={handleCloseChangePassword} />
+            )}
+          </div>
         </div>
       </div>
     </div>
