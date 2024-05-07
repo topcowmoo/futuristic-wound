d5fe521c7c774f8c90fbd69d864efb0a8cc7343b
Daniel John <dcampo85@gmail.com>
2024-05-06 20:05:21 -0400

    desktop rescaling 4

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -2,21 +2,21 @@ import { Link } from "react-router-dom";
 
 function Success() {
   return (
-    <div className="flex justify-center items-center h-[620px]">
-      <div className="flex flex-col m-auto justify-center items-center relative top-[50px]">
-        <div className="[background:#FFF] shadow-[0px_4px_48px_0px_rgba(0,0,0,0.25)] rounded-2xl w-[300px] h-[400px] fixed -z-50"></div>
-        <h1 className="text-center text-2xl font-bold mx-24 ">
+    <div className="2xl:flex 2xl:justify-center 2xl:items-center 2xl:h-[620px]">
+      <div className="2xl:flex 2xl:flex-col 2xl:m-auto 2xl:justify-center 2xl:items-center 2xl:relative 2xl:top-[50px]">
+        <div className="2xl:[background:#FFF] 2xl:shadow-[0px_4px_48px_0px_rgba(0,0,0,0.25)] 2xl:rounded-2xl 2xl:w-[300px] 2xl:h-[400px] 2xl:fixed 2xl:-z-50"></div>
+        <h1 className="2xl:text-center 2xl:text-2xl 2xl:font-bold 2xl:mx-24">
           Thank you for your purchase!
         </h1>
         <Link
           to="/home"
-          className="bg-blue-500 text-white p-2 rounded-xl w-24 text-center mt-8"
+          className="2xl:bg-blue-500 2xl:text-white 2xl:p-2 2xl:rounded-xl 2xl:w-24 2xl:text-center 2xl:mt-8"
         >
           Home
         </Link>
         <Link
           to="/adventure"
-          className="bg-blue-500 text-white p-2 rounded-xl text-center mt-2"
+          className="2xl:bg-blue-500 2xl:text-white 2xl:p-2 2xl:rounded-xl 2xl:text-center 2xl:mt-2"
         >
           Back to Map
         </Link>
